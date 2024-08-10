9fa83d77a885119b8e0edb4f1be520aa29630cca
liaoke123 <104013620+liaoke01@users.noreply.github.com>
2024-08-10 17:27:07 +0800

    Update ipkg-make-index.sh

--- .github/workflows/ipkg-make-index.sh
+++ .github/workflows/ipkg-make-index.sh
@@ -18,12 +18,12 @@ for pkg in `find $pkg_dir -name '*.ipk' | sort`; do
 	[[ "$name" = "libc" ]] && continue
 	echo "Generating index for package $pkg" >&2
 	file_size=$(stat -L -c%s $pkg)
-	#sha256sum=$(echo "" | $MKHASH sha256 $pkg)
+	sha256sum=$(echo "" | $MKHASH sha256 $pkg)
 	# Take pains to make variable value sed-safe
 	sed_safe_pkg=`echo $pkg | sed -e 's/^\.\///g' -e 's/\\//\\\\\\//g'`
 	tar -xzOf $pkg ./control.tar.gz | tar xzOf - ./control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
 Size: $file_size\\
-SHA256sum: 1\\
+SHA256sum: $sha256sum\\
 Description:/"
 	echo ""
 done
